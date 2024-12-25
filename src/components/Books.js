a646c5c9d60537463a27cc0e3ea8e35483530a61
Karan-Bharti1 <karan.codes1@gmail.com>
2024-12-25 09:10:02 +0530

    feat:Added delete functionality to books data

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,18 +1,41 @@
+import { useState } from 'react';
 import useFetch from '../useFetch';
 
 const Books = () => {
   const { data, loading, error } = useFetch(
     'https://be-4-assignment1-silk.vercel.app/'
   );
+  const [successMessage,setSuccessMessage]=useState()
+  const handleDelete=async(bookId)=>{
+try {
+  const response=await fetch(`https://be-4-assignment1-silk.vercel.app/books/${bookId}`,{
+    method:"DELETE"
+  })
+  if(!response.ok){
+    throw "Failed to delete book data"
+  }
+  const res=await response.json()
+  if(res){
+setSuccessMessage("Data Delete fromthe database")
+
+window.location.reload()
+  }
+} catch (error) {
+  console.log(error)
+}
+  }
   console.log(data);
   const displayData = data?.map((book) => (
-    <li key={book._id}>{book.title} </li>
+    <li key={book._id}>{book.title} <button onClick={()=>handleDelete(book._id)}>Delete</button></li>
   ));
   return (
     <>
       <h2>All Books</h2>
   {  loading&&  <p>Loading...</p>}
       {displayData}
+      {
+        successMessage
+      }
     </>
   );
 };
